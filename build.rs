3695b7181a62d4b255c06962e60899b45dc92d27
Frank Plowman <post@frankplowman.com>
2023-10-09 12:45:33 +0100

    Get cross-compilation working

--- build.rs
+++ build.rs
@@ -1,16 +1,22 @@
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    bindgen();
-    build();
+fn is_crosscompile() -> bool {
+    env::var("HOST").unwrap() != env::var("TARGET").unwrap()
 }
 
-fn bindgen() {
+fn main() {
+    let build_path = build();
+    bindgen(&build_path);
+}
+
+fn bindgen(build_path: &PathBuf) {
     // Invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    let bindings = bindgen::Builder::default()
+    let mut builder = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
         .header("wrapper.h")
         // Only generate bindings for the arm_ functions and variables
         .allowlist_function("arm_.*")
@@ -18,12 +24,25 @@ fn bindgen() {
         // Where to look for headers
         .clang_arg("-ICMSIS-DSP/Include")
         .clang_arg("-ICMSIS_5/CMSIS/Core/Include")
+        // Presume the compiler works
+        // This is necessary because the compiler checks rely on
+        // having a standard library present
+        .clang_arg("-DCMAKE_C_COMPILER_WORKS=1")
         // Don't use the rust standard library
         .use_core()
         // Invalidate the built crate if any of the included header files change
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings");
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks));
+
+    if is_crosscompile() {
+        builder = builder
+            .clang_arg("-Ipicolibc/newlib/libc/include")
+            .clang_arg(format!(
+                "-I{}/build/picolibc/picolibc/include",
+                build_path.display()
+            ))
+    }
+
+    let bindings = builder.generate().expect("Unable to generate bindings");
 
     // Write the bindings to $OUT_DIR/bindings.rs
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -32,11 +51,19 @@ fn bindgen() {
         .expect("Couldn't write bindings!");
 }
 
-fn build() {
-    let dst = cmake::Config::new(".").build_target("CMSISDSP").build();
+fn build() -> PathBuf {
+    let mut config = cmake::Config::new(".");
+    config.build_target("CMSISDSP");
+    if is_crosscompile() {
+        // @TODO: This is a bit hacky.
+        //        We should probably set this sort of option up in a toolchain file.
+        config.define("CMAKE_EXE_LINKER_FLAGS", "--specs=nosys.specs");
+    }
+    let dst = config.build();
     println!(
         "cargo:rustc-link-search=native={}/build/CMSIS-DSP/Source/",
         dst.display()
     );
     println!("cargo:rustc-link-lib=static=CMSISDSP");
+    dst
 }
